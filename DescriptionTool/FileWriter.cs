f7c13f25896d920aa5153381010231b1d36b6b52
frank-guo <frank_guoguo@hotmail.com>
2015-04-25 23:39:49 -0700

    Add FileWriter.cs and Logger.cs

---
+++ DescriptionTool/FileWriter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DescriptionTool
+{
+    class FileWriter
+    {
+        private string _outputFile;
+
+        public FileWriter(string outputFile)
+        {
+            _outputFile = outputFile;
+
+            if (File.Exists(outputFile))
+            {
+                File.Delete(outputFile);
+            }
+        }
+
+        public void write(string contentToWrite)
+        {
+        save:
+            try
+            {
+                File.AppendAllText(_outputFile, contentToWrite);
+            }
+            catch (Exception e)
+            {
+                DialogResult result = DialogResult.Abort;
+                try
+                {
+                    result = MessageBox.Show("Please contact the developers with the"
+                      + " following information:\n\n" + e.Message + e.StackTrace,
+                      "Application Error", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop);
+                    if (result == DialogResult.Retry)
+                    {
+                        goto save;
+                    }
+                }
+                finally
+                {
+                    if (result == DialogResult.Abort)
+                    {
+                        Application.Exit();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DescriptionTool/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescriptionTool
+{
+    class Logger
+    {
+        private string _logfile;
+        private static string Filename = "log.txt";
+
+        public Logger(string logFolder)
+        {
+            _logfile = logFolder + "\\" + Filename;
+        }
+
+        public void writeLog(string line)
+        {
+            if (File.Exists(_logfile))
+            {
+                File.Delete(_logfile);
+            }
+
+
+            using (StreamWriter log = File.CreateText(_logfile))
+            {
+                log.WriteLine(line);
+            }
+        }
+    }
+}
